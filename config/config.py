b7342107f58157cf99faff0fb085f62b50fbe38d
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-10-10 23:09:14 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -129,7 +129,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(
 )  # Remember to give value in bytes
 
 TG_VIDEO_FILESIZE_LIMIT = int(
-    getenv("TG_VIDEO_FILESIZE_LIMIT", "1096810496")
+    getenv("TG_VIDEO_FILESIZE_LIMIT", "1530920960")
 )  # Remember to give value in bytes
 
 # Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
